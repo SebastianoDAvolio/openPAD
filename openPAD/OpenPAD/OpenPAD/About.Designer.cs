c052fa3ddb12195ecad15fe28e1239662457520f
costaaaaa <61553135+costaaaaa@users.noreply.github.com>
2021-02-09 22:46:07 +0100

    Add files via upload

---
+++ openPAD/OpenPAD/OpenPAD/About.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace OpenPAD
+{
+    partial class About
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
+            this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.labelProductName, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.labelVersion, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.labelCopyright, 1, 2);
+            this.tableLayoutPanel.Controls.Add(this.textBoxDescription, 1, 3);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(6, 9);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 4;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 62.5F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(417, 265);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoPictureBox.Image = global::OpenPAD.Properties.Resources.logoPng;
+            this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 4);
+            this.logoPictureBox.Size = new System.Drawing.Size(131, 259);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.logoPictureBox.TabIndex = 12;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelProductName.Location = new System.Drawing.Point(143, 0);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(271, 17);
+            this.labelProductName.TabIndex = 19;
+            this.labelProductName.Text = "Nome prodotto";
+            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelVersion.Location = new System.Drawing.Point(143, 33);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(271, 17);
+            this.labelVersion.TabIndex = 0;
+            this.labelVersion.Text = "Versione";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCopyright.Location = new System.Drawing.Point(143, 66);
+            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(271, 17);
+            this.labelCopyright.TabIndex = 21;
+            this.labelCopyright.Text = "Copyright";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxDescription.Location = new System.Drawing.Point(143, 102);
+            this.textBoxDescription.Margin = new System.Windows.Forms.Padding(6, 3, 3, 3);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.ReadOnly = true;
+            this.textBoxDescription.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDescription.Size = new System.Drawing.Size(271, 160);
+            this.textBoxDescription.TabIndex = 23;
+            this.textBoxDescription.TabStop = false;
+            this.textBoxDescription.Text = "OpenPAD è un semplice word processor con supporto a file txt e rtf";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Chiudi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 312);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.Padding = new System.Windows.Forms.Padding(9);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.About_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelCopyright;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ openPAD/OpenPAD/OpenPAD/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpenPAD
+{
+    public partial class Form1 : Form
+    {
+        string CMDpath;
+        string filename;
+        bool newFile=true;
+        public Form1(string path)
+        {
+            CMDpath = path;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fontViewer.Text = document.Font.Name;
+            if (CMDpath!="NOPATH")
+            {
+                if (CMDpath.Contains(".rtf"))
+                {
+                    Read(CMDpath, true);
+                }
+                else {
+                    Read(CMDpath,false);
+                }
+            }
+        }
+
+        private void Read(string addr, bool asRtf)
+        {
+            if(asRtf){
+                try
+                {
+                    document.LoadFile(addr);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Errore durante il caricamento del file rtf, verra` tentato il caricamento come file di testo semplice","Errore durante la lettura del file rtf",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    Read(addr, false);
+                }
+                
+            }
+            else
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(addr);
+                    document.Text = sr.ReadToEnd();
+                    sr.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore durante il caricamento del file di testo, il file system ha presentato il seguente errore: "+ex.Message,"Errore durante la lettura del file di testo",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+
+            }
+        }
+
+        private void apriToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newFile = false;
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                //filename = openFileDialog1.SafeFileName;
+                if (filename.Contains(".rtf"))
+                {
+                    Read(openFileDialog1.FileName,true);
+                }
+                else
+                {
+                    Read(openFileDialog1.FileName, false);
+                }
+                
+            }
+        }
+
+        private void nuovoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewDoc();
+            newFile = true;
+        }
+
+        private void NewDoc()
+        {
+            if (document.Text != "") {
+                document.Clear();
+                filename = "";
+            }
+            //inserire conferma operazione
+        }
+
+        private void esciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void informazioniToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About ab = new About();
+            ab.ShowDialog();
+        }
+
+        private void fontSelector_Click(object sender, EventArgs e)
+        {
+            DialogResult res = mainFont.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                fontViewer.Text = mainFont.Font.Name;
+                if (document.SelectedText.Length==0)
+                {
+                    document.Font = mainFont.Font;
+                }
+                else
+                {
+                    document.SelectionFont = mainFont.Font;
+                }
+            }
+        }
+
+        private void salvaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (newFile == true)
+            {
+                if (CMDpath != "NOPATH")
+                {
+                    if (CMDpath.Contains(".rtf"))
+                    {
+                        saveWithName(true);
+                    }
+                    else
+                    {
+                        saveWithName(false);
+                    }
+                }
+            }
+            else
+            {
+                save(true);
+            }
+            
+        }
+
+        private void salvaConNomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                if (CMDpath.Contains(".rtf"))
+                {
+                    saveWithName(true);
+                }
+                else
+                {
+                    saveWithName(false);
+                }
+        }
+
+
+        private void save(bool asRtf) //non funziona
+        {
+            if (asRtf)
+            {
+                //salvataggio file rtf
+            }
+            else
+            {
+                try
+                {
+                    if (CMDpath != "NOPATH")
+                    {
+                        if (CMDpath.Contains(".rtf"))
+                        {
+                            Read(CMDpath, true);
+                        }
+                        else
+                        {
+                            Read(CMDpath, false);
+                        }
+                    }
+                    StreamWriter myStream = new StreamWriter(CMDpath);
+                    SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                    if (myStream != null)
+                    {
+                        myStream.Write(document.Text);
+                        myStream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore durante il salvataggio del file di testo, Errore:" + ex.Message, "Errore durante la scritture del file di testo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+
+        }
+
+        private void saveWithName(bool asRtf) //non salva il font e grassetto/corsivo/sottolineato
+        {
+            if (asRtf)
+            {
+                //salvataggio file rtf con nome
+            }
+            else
+            {
+                try
+                {
+                    Stream myStream;
+                    SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                    var fileContent = string.Empty;
+                    saveFileDialog1.Filter = "rtf files (*.rtf)|*.rtf|txt files (*.txt)|*.txt| All files (*.*)|*.*";
+                    saveFileDialog1.FilterIndex = 2;
+                    saveFileDialog1.RestoreDirectory = true;
+
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        if ((myStream = saveFileDialog1.OpenFile()) != null)
+                        {
+                            using (StreamWriter writer = new StreamWriter(myStream))
+                            {
+                                writer.Write(document.Text);
+                                writer.Close();
+                            }
+                            myStream.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore durante il salvataggio del file di testo, Errore:" + ex.Message,"Errore durante la scritture del file di testo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
